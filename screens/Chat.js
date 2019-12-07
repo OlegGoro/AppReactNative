b9e5daee2f1d1e086b744430c321f9fd7741896e
Your Name <1650876@gmail.com>
2019-12-07 03:46:14 +0300

    Chat WIP

--- screens/Chat.js
+++ screens/Chat.js
@@ -5,42 +5,111 @@ import {SafeAreaView} from "react-navigation";
 
 const int = ExampleChat =>{};
 
+var REQUEST_URL = "http://192.168.0.107:8000/api/v1/messages/?myname=Slava&chatsession=87"
+
+var previousCount
+
 export default class ExampleChat extends React.Component {
+
     state = {
         messages: [],
     }
 
-    componentWillMount() {
-        this.setState({
-            messages: [
-                {
-                    _id: 1,
-                    text: 'Hello developer',
-                    createdAt: new Date(),
-                    user: {
-                        _id: 2,
-                        name: 'React Native',
-                        avatar: 'https://placeimg.com/140/140/any',
-                    },
-                },
-                {
-                    _id: 2,
-                    text: 'Hello developer current',
-                    createdAt: new Date(),
-                    user: {
-                        _id: 1,
-                        name: 'React Native',
-                        avatar: 'https://placeimg.com/140/140/any',
-                    },
-                },
-            ],
+     _fetchData = () => {
+        fetch(REQUEST_URL)
+        .then((response) => response.json())
+        .then((responseJson) => {
+         objBeforeTrasnform = responseJson
+         newObj = []
+         for (let k in objBeforeTrasnform) {
+           imagefile = objBeforeTrasnform[k].image.split('/').slice(9,10)
+           trueimage = "http://192.168.0.107:8000/static/media/" + imagefile
+           var messegeuserid
+           if (objBeforeTrasnform[k].sender == "Olga"){
+             messegeuserid = 2
+           } else {
+             messegeuserid = 1
+           }
+           newObj.push({_id: k, text: objBeforeTrasnform[k].message, createdAt: objBeforeTrasnform[k].timestamp, user: {_id: messegeuserid, name: objBeforeTrasnform[k].sender, avatar: trueimage}});
+         }
+          previousCount = 0;
+          for(var prop in newObj) {
+              previousCount++;
+          }
+         this.setState({
+           messages: newObj,
+         });
         })
+        .catch((error) =>{
+         //this.closemyclaim()
+        });
     }
 
-    onSend(messages = []) {
+    _updateData = () => {
+       fetch(REQUEST_URL)
+       .then((response) => response.json())
+       .then((responseJson) => {
+        objBeforeTrasnform = responseJson
+        newObj = []
+        for (let k in objBeforeTrasnform) {
+          imagefile = objBeforeTrasnform[k].image.split('/').slice(9,10)
+          trueimage = "http://192.168.0.107:8000/static/media/" + imagefile
+          var messegeuserid
+          if (objBeforeTrasnform[k].sender == "Olga"){
+            messegeuserid = 2
+          } else {
+            messegeuserid = 1
+          }
+
+          newObj.push({_id: k, text: objBeforeTrasnform[k].message, createdAt: objBeforeTrasnform[k].timestamp, user: {_id: messegeuserid, name: objBeforeTrasnform[k].sender, avatar: trueimage}});
+        }
+
+        //const filteredObj = newObj.filter((item) => item._id > previousCount);
+        var filteredObj = newObj.filter(function (el) {
+          return el._id >= previousCount
+        });
         this.setState(previousState => ({
-            messages: GiftedChat.append(previousState.messages, messages),
+            messages: GiftedChat.append(previousState.messages, filteredObj),
         }))
+        previousCount = 0;
+        for(var prop in newObj) {
+            previousCount++;
+        }
+       })
+       .catch((error) =>{
+        //this.closemyclaim()
+       });
+   }
+
+    _sendMessage = async (messages) => {
+        const data = new FormData();
+        data.append("message", "messages")
+        data.append("sender", "Slava")
+        data.append("receiver", "Olga")
+        data.append("chatsession", 87)
+        fetch("http://192.168.0.107:8000/api/v1/addmessage/", {
+          method: "POST",
+          headers: {
+            'Content-Type': 'multipart/form-data',
+          },
+          body: data,
+        }).then(res => {
+    console.log(res)
+  });
+  }
+
+    componentWillMount() {
+         this._fetchData()
+         setInterval(this._updateData, 2000)
+
+    }
+
+
+
+
+    onSend(messages = []) {
+        this._sendMessage(messages)
+        this._updateData()
     }
 
     render() {
